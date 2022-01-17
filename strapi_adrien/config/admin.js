f68d61c1309af269dab3a15d8f4f744e968c6cf2
Adrien <59483650+adrien24@users.noreply.github.com>
2022-01-17 15:38:53 +0100

    config postgres

--- strapi_adrien/config/admin.js
+++ strapi_adrien/config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '582fa5ed62ae66185e4b0975ce5feeda'),
+    secret: env('ADMIN_JWT_SECRET', '4cbc5eae7015e7cb7766ff9024280e66'),
   },
 });
